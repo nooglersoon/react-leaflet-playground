57890fbeb356752c8d9993710ae26a73e25fbd20
nooglersoon <nooglersoon@outlook.co.id>
2022-09-30 11:10:16 +0700

    init project

---
+++ anyname.d.ts
@@ -0,0 +1,16 @@
+// anyname.d.ts - place this anywhere in your project
+declare namespace React {
+  interface HTMLAttributes<T> {
+    // Preact supports using "class" instead of "classname" - need to teach typescript
+    class?: string;
+  }
+
+  interface SVGProps<T> {
+    class?: string;
+  }
+
+  interface SVGProps<T> {
+    class?: string;
+  }
+
+}
